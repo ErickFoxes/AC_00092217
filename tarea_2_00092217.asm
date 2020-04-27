8a95ee834a32ff75682a1e937afa8040575e754f
ErickFoxes <37157947+ErickFoxes@users.noreply.github.com>
2020-04-27 01:26:32 -0300

    Ejercicio 3 terminado
    ejercicio 3 hecho y tarea fnalizada

--- tarea_2_00092217.asm
+++ tarea_2_00092217.asm
@@ -1,6 +1,6 @@
 	org 100h
 ;------EJERCICIO 1---------
-	mov cx, 00000000b;Carnet 00092217 = 21 y 21/5=4.5 redondeando 4
+	mov cx, 0d;Carnet 00092217 = 21 y 21/5=4.5 redondeando 4
 	mov ax,cx
 	ADD ax,9d
 	ADD ax,2d
@@ -32,11 +32,11 @@
 	mov [209h], cl
 ;------EJERCICIO 1---------
 ;------EJERCICIO 2---------
-	mov ax, 0000h;
+	mov ax, 0d;
 	mov bx, 210h 
 	mov al, 2d
 	mov cx, 2d
-	
+
 lup: mul  cx
         mov [bx], ax
         cmp ah, 00h
@@ -54,4 +54,24 @@ menor:	cmp bx, 21Fh
 
 
 ;------EJERCICIO 2---------
+;------EJERCICIO 3---------
+        mov ax, 0d
+        mov [220h], ax
+        mov ax, 1d
+        mov [221h], ax
+        mov bx, 222h
+	
+formu:	mov ax, 0000h
+        mov [bx], ax
+	sub bx, 2h
+        mov al, [bx]
+	add bx, 1h
+        add ax, [bx]
+        add bx, 1h
+        mov [bx], ax
+        add bx, 1h
+        cmp bx, 22Fh
+        jb formu; menor que
+
+;------EJERCICIO 3---------
 int 20h
