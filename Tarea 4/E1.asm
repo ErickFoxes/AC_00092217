f2b2cc9f566681db052f5979c94ea95f2f556c54
ErickFoxes <37157947+ErickFoxes@users.noreply.github.com>
2020-06-18 03:17:57 -0300

    Siempre me muestra me recupero :/

--- Tarea 4/E1.asm
+++ Tarea 4/E1.asm
@@ -1,18 +1,39 @@
 org 	100h
 
 section .text
-
+	call	w_meni;00092217 9+2+2+1+7 = 21 / 4.2 prox 4 me recupero
 	xor 	si, si 	;lo mimso que: mov si, 0000h
+	
 lupi:	call 	kb
 	cmp 	al, "$"
-	je	mostrar
+	je	ceros
+	sub     al, 30h
 	mov	[300h+si], al ; CS:0300h en adelante
 	inc 	si
 	jmp 	lupi
 
-mostrar:mov	byte [300h+si], "$"
-	call 	w_strng
+ceros:  mov     bx, 0d
+        mov     ax, 0d
 
+sumar:	add	al,[300h+bx]
+	inc	bx
+	cmp     bx, 5d
+	jb	sumar
+	mov     [310h], al
+        mov     [317h], bl
+        mov     cl, bl
+        div     cl
+        mov     [330h], al
+
+mostrar:mov dx, nl     
+	call 	w_strng
+	cmp	al,4d
+	je	nota4
+
+nota4:	mov 	dx, msg2
+        jmp     mostra2
+
+mostra2:call 	w_strng
 	call 	kb	; solo detenemos la ejecución
 	int 	20h
 
@@ -21,11 +42,21 @@ texto:	mov 	ah, 00h
 	int 	10h
 	ret
 
-kb: 	mov	ah, 7h
+kb: 	mov	ah, 1h
 	int 	21h
 	ret
 
 w_strng:mov	ah, 09h
-	mov 	dx, 300h
 	int 	21h
 	ret
+
+
+w_meni:	mov dx,  msg
+	mov ah, 9
+	int 21h
+	ret
+
+section .data
+msg	db 	"Digite cinco datos $";Haiku
+msg2	db 	"Me recupero $"
+nl	db 	0xA, 0xD, "$"
