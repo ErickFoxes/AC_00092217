8a96b8ae924bce01a8d6071599ff8384d2a4dadc
ErickFoxes <37157947+ErickFoxes@users.noreply.github.com>
2020-06-23 15:16:34 -0300

    Letra F(Fernando) terminada

--- Tarea 5/Tarea_5_00092217.asm
+++ Tarea 5/Tarea_5_00092217.asm
@@ -19,6 +19,34 @@ section .text
 	mov 	di, 45d ; Y -> Fila
 	call 	linea_3
 
+	mov 	si, 120d ; X -> Columna
+	mov 	di, 69d ; Y -> Fila
+	call 	linea_2
+
+	mov 	si, 120d ; X -> Columna
+	mov 	di, 69d ; Y -> Fila
+	call 	linea_5
+
+	mov 	si, 120d ; X -> Columna
+	mov 	di, 93d ; Y -> Fila
+	call 	linea_2
+
+	mov 	si, 120d ; X -> Columna
+	mov 	di, 117d ; Y -> Fila
+	call 	linea_2
+
+	mov 	si, 156d ; X -> Columna
+	mov 	di, 93d ; Y -> Fila
+	call 	linea_7
+
+	mov 	si, 120d ; X -> Columna
+	mov 	di,117d ; Y -> Fila
+	call 	linea_9
+
+	mov 	si, 106d ; X -> Columna
+	mov 	di, 155d ; Y -> Fila
+	call 	linea_10
+
 	call 	kb		; Utilizamos espera de alguna tecla
 
 	int 	20h
@@ -39,7 +67,7 @@ lupi_v1:mov 	cx, si ; Columna
 	add 	dx, di
 	call 	pixel
 	inc 	di
-	cmp 	di, 155d
+	cmp 	di, 156d
 	jne 	lupi_v1
 	ret
 
@@ -61,6 +89,46 @@ lupi_v3:mov 	cx, si ; Columna
 	inc 	di
 	cmp 	di, 69d
 	jne 	lupi_v3
+	ret
+
+linea_5:
+lupi_v5:mov 	cx, si ; Columna 
+	mov	dx, 0d ; Fila
+	add 	dx, di
+	call 	pixel
+	inc 	di
+	cmp 	di, 93d
+	jne 	lupi_v5
+	ret
+
+linea_7:
+lupi_v7:mov 	cx, si ; Columna 
+	mov	dx, 0d ; Fila
+	add 	dx, di
+	call 	pixel
+	inc 	di
+	cmp 	di, 117d
+	jne 	lupi_v7
+	ret
+
+linea_9:
+lupi_v9:mov 	cx, si ; Columna 
+	mov	dx, 0d ; Fila
+	add 	dx, di
+	call 	pixel
+	inc 	di
+	cmp 	di, 155d
+	jne 	lupi_v9
+	ret
+
+linea_10: 
+lupi_h10:mov 	cx, 0d ; Columna 
+	add 	cx, si
+	mov	dx, di ; Fila
+	call 	pixel
+	inc 	si
+	cmp 	si, 120d
+	jne 	lupi_h10
 	ret
 
 
